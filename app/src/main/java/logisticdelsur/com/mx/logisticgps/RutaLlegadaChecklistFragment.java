e49189f6baa81f2931e0ad601e3181e7c824aab1
dapv67 <alvarodperezv@gmail.com>
2021-02-10 11:12:19 -0600

    V.1 Front de la app

---
+++ app/src/main/java/logisticdelsur/com/mx/logisticgps/RutaLlegadaChecklistFragment.java
@@ -0,0 +1,99 @@
+package logisticdelsur.com.mx.logisticgps;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RutaLlegadaChecklistFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RutaLlegadaChecklistFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public RutaLlegadaChecklistFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RutaLlegadaChecklistFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RutaLlegadaChecklistFragment newInstance(String param1, String param2) {
+        RutaLlegadaChecklistFragment fragment = new RutaLlegadaChecklistFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ruta_llegada_checklist, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCheckLlegada = view.findViewById(R.id.btn_checkLlegadaRuta);
+        btnCheckLlegada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Programación de mantenimientos");
+                builder.setMessage("¿Quieres programar un mantenimiento?");
+
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Navigation.findNavController(v).navigate(R.id.action_rutaLlegadaChecklistFragment_to_rutaMantenimientoFragment);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Navigation.findNavController(v).navigate(R.id.action_rutaLlegadaChecklistFragment_to_homeFragment);
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+}
